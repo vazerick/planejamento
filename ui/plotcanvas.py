26e53b0c64698cefde5797218fbb5fba83d68d23
Erick Vaz <erickvaz.rs@gmail.com>
2019-04-26 13:56:56 -0300

    Ajuste

--- ui/plotcanvas.py
+++ ui/plotcanvas.py
@@ -1,14 +1,8 @@
-import sys
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, QPushButton
-from PyQt5.QtGui import QIcon
-
+from PyQt5.QtWidgets import QSizePolicy
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 
-import random
 
 
 class PlotCanvas(FigureCanvas):
